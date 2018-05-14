6214ab57a9edb6a41fdb021b3bdd167f9b59a769
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-14 19:01:20 -0400

    updated correspondingly

--- part1/oj-client/src/app/app.module.ts
+++ part1/oj-client/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { routing } from "./app.routes";
+
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 
 import { DataService } from "./services/data.service";
-import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { ProblemDetailComponent } from './components/problem-detail/problem-deta
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [{
     provide: "data",
