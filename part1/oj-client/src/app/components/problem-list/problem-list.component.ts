98606a6227a2db433fc2c1514e68e014c44aa3be
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-14 16:08:13 -0400

    changed page contents and tried to add problem-list component

--- part1/oj-client/src/app/components/problem-list/problem-list.component.ts
+++ part1/oj-client/src/app/components/problem-list/problem-list.component.ts
@@ -1,19 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Problem } from "../../models/problem.model";
+
+const PROBLEMS: Problem[] = [
+  {
+    id: 1,
+    name: "Two Sum",
+    desc: "Given an array ...",
+    difficulty: "easy"
+  },
+  {
+    id: 2,
+    name: "3Sum",
+    desc: "Given an array S of n integers ...",
+    difficulty: "medium"
+  },
+  {
+    id: 3,
+    name: "4Sum",
+    desc: "Given ..",
+    difficulty: "medium"
+  }]
 
 @Component({
   selector: 'app-problem-list',
   template: `
-    <p>
-      problem-list Works!
-    </p>
+    <div class="container">
+      <div class="list-group">
+        <a class="list-group-item" *ngFor="let problem of problems">
+          <span>{{problem.difficulty}}</span>
+          <strong>{{problem.id}}. {{problem.name}}</strong>
+        </a>
+      </div>
+    </div>
   `,
   styles: []
 })
 export class ProblemListComponent implements OnInit {
 
+  problems: Problem[];
+
   constructor() { }
 
   ngOnInit() {
+    this.problems = PROBLEMS;
   }
 
 }
