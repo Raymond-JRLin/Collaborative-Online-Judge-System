1698d33b79b7acd80fd77053cadae2386f0e491f
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-15 19:57:36 -0400

    added mongodb

--- part2/oj-server/server.js
+++ part2/oj-server/server.js
@@ -1,6 +1,9 @@
 var express = require("express") // import express package
 var app = express()
-var restRouter = require("./routes/rest")
+var restRouter = require("./routes/rest");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://<junrui>:<coj>@ds021731.mlab.com:21731/coj")
 
 app.use("/api/v1", restRouter)
 
