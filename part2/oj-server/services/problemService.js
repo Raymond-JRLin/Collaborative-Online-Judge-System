fe69acf783adfe883986e1e1d9f133df74e69b8c
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-15 19:01:17 -0400

    deal with different services

--- part2/oj-server/services/problemService.js
+++ part2/oj-server/services/problemService.js
@@ -36,6 +36,26 @@ var getProblems = function () {
   });
 }
 
+var getProblem = function (id) {
+  return new Promise(function(resolve, reject) {
+    resolve(problems.find(problem => problem.id === id));
+  });
+}
+
+var addProblem = function (newProblem) {
+  return new Promise((resolve, reject) => {
+    if (problems.find(problem => problem.name === newProblem.name)) {
+      reject("Problem already exist!");
+    } else {
+      newProblem.id = problems.length + 1;
+      problems.push(newProblem);
+      resolve(newProblem);
+    }
+  });
+}
+
 module.exports = {
-  getProblems: getProblems
+  getProblems: getProblems,
+  getProblem: getProblem,
+  addProblem: addProblem
 };
