4a8a38f118eeb9e5db37404d1937184b784b691e
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-15 18:58:42 -0400

    added server js

--- part2/oj-server/server.js
+++ part2/oj-server/server.js
@@ -1,10 +1,8 @@
 var express = require("express") // import express package
-
 var app = express()
+var restRouter = require("./routes/rest")
 
-app.get('/', function(req, res) {
-  res.send('Hello Express haha')
-})
+app.use("/api/v1", restRouter)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
