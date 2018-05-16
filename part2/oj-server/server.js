8769cd61d6c024a02ce1fd958c3605c8b15811c1
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-15 21:32:06 -0400

    modified server correspondingly

--- part2/oj-server/server.js
+++ part2/oj-server/server.js
@@ -1,11 +1,12 @@
 var express = require("express") // import express package
 var app = express()
 var restRouter = require("./routes/rest");
+var indexRouter = require("./routes/index");
 var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://junrui:coj@ds021731.mlab.com:21731/coj");
-
-app.use("/api/v1", restRouter)
+app.use('/', indexRouter); // root page, route to index
+app.use("/api/v1", restRouter);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
