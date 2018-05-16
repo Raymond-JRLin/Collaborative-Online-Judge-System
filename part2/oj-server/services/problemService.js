e38eab77e9d3b7fdcb53e6d15520c9d308ef86f2
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-15 21:00:08 -0400

    deleted constant mock problems

--- part2/oj-server/services/problemService.js
+++ part2/oj-server/services/problemService.js
@@ -1,35 +1,3 @@
-var problems = [
-  {
-    id: 1,
-    name: "Two Sum",
-    desc: "Given an array ...",
-    difficulty: "easy"
-  },
-  {
-    id: 2,
-    name: "3Sum",
-    desc: "Given an array S of n integers ...",
-    difficulty: "medium"
-  },
-  {
-    id: 3,
-    name: "4Sum",
-    desc: "Given ..",
-    difficulty: "medium"
-  },
-  {
-    id: 4,
-    name: "Triangle count",
-    desc: 'Given an arraay of integers, how many three numbers',
-    difficulty: "hard"
-  },
-  {
-    id: 5,
-    name: "Sliding Window Maximum",
-    desc: "Given an array of n interger with duplicate number",
-    difficulty: "super"
-  }];
-
 var ProblemModel = require('../models/problemModel');
 
 var getProblems = function () {
