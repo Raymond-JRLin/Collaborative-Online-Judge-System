3c5f4407c47ee7d7772df2a19a88af5640f51fa5
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-16 16:56:28 -0400

    correction

--- part3/oj-client/src/app/components/navbar/navbar.component.ts
+++ part3/oj-client/src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  tittle = "Collaborative Online Judge System";
+  title = "Collaborative Online Judge System";
 
   username = "Raymond";
 
