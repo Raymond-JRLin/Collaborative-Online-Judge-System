1dc009d2226146db339c800ed83356df5b4bfc77
Junrui Lin <raymond.jr.lin@gmail.com>
2018-05-16 16:24:02 -0400

    keep doing

---
+++ part3/oj-client/src/app/mock-problems.ts
@@ -0,0 +1,33 @@
+import { Problem } from "./models/problem.model";
+
+export const PROBLEMS: Problem[] = [
+  {
+    id: 1,
+    name: "Two Sum",
+    desc: "Given an array ...",
+    difficulty: "easy"
+  },
+  {
+    id: 2,
+    name: "3Sum",
+    desc: "Given an array S of n integers ...",
+    difficulty: "medium"
+  },
+  {
+    id: 3,
+    name: "4Sum",
+    desc: "Given ..",
+    difficulty: "medium"
+  },
+  {
+    id: 4,
+    name: "Triangle count",
+    desc: 'Given an arraay of integers, how many three numbers',
+    difficulty: "hard"
+  },
+  {
+    id: 5,
+    name: "Sliding Window Maximum",
+    desc: "Given an array of n interger with duplicate number",
+    difficulty: "super"
+  }]
---
+++ part3/oj-server/server.js
@@ -0,0 +1,15 @@
+var express = require("express") // import express package
+var app = express()
+var restRouter = require("./routes/rest");
+var indexRouter = require("./routes/index");
+var mongoose = require("mongoose");
+var path = require("path");
+
+mongoose.connect("mongodb://junrui:coj@ds021731.mlab.com:21731/coj");
+app.use(express.static(path.join(__dirname, '../public'))); // go public to find static files
+app.use('/', indexRouter); // root page, route to index
+app.use("/api/v1", restRouter);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
