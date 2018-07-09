b432a6ceaaf048ce479999a1f55507d36d14eca9
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-09 17:18:33 -0400

    modified ace

--- part3/oj-client/src/app/components/editor/editor.component.ts
+++ part3/oj-client/src/app/components/editor/editor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ace: any;
+
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  editor: any;
+
+  defaultContent = {
+    'Java': `public class Solution {
+      public static void main(String[] args) {
+        // Type your soluton here
+      }
+    }`
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.editor = ace.edit('editor');
+    this.editor.setTheme('ace/theme/eclipse');
+    this.editor.getSession().setMode('ace/mode/java');
+    this.editor.setValue(this.defaultContent['Java']);
+    this.editor.$blockScrolling = Infinity;
   }
 
 }
