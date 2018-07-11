c150cc91bbf712b159116938d1bbf26f16941bcb
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-10 20:43:45 -0400

    update pipe service for search box

--- part3/oj-client/src/app/components/navbar/navbar.component.ts
+++ part3/oj-client/src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { FormControl } from "@angular/forms";
 import { Subscription } from "rxjs/Subscription";
 import { Router } from "@angular/router";
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-navbar',
@@ -29,6 +30,7 @@ export class NavbarComponent implements OnInit {
 
     this.subscription = this.searchBox
                             .valueChanges
+                            .debounceTime(200) // catch events every 200ms
                             .subscribe(
                               term => {
                                 this.input.changeInput(term);
